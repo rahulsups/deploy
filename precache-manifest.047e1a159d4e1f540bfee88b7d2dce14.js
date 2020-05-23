85a79462ef9557fcce1bb552e9fe2dd29b57d231
rahulsups <rahul.27bok@gmail.com>
2020-05-23 16:39:18 +0530

    Updates

---
+++ precache-manifest.047e1a159d4e1f540bfee88b7d2dce14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99e33edd6f2fa2dd479ba0e4a9f4f43",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "b289d73eb2f1219c16e1",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24acc32fdd904752fb07",
+    "url": "/deploy/static/js/2.8d56a7c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.8d56a7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b289d73eb2f1219c16e1",
+    "url": "/deploy/static/js/main.270cd757.chunk.js"
+  },
+  {
+    "revision": "c8f5bd84c363d903ebe6",
+    "url": "/deploy/static/js/runtime-main.b6376735.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
